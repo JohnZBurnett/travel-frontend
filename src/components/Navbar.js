91dda8fc496fad334fc6548ba4aa9982613e1355
JohnZBurnett <tuninator@gmail.com>
2018-07-30 09:46:49 -0400

    updating navbar step 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  console.log("Navbar Props: ", props);
+
+  function displayUserActionsPanel() {
+    if (props.userLoggedIn === false) {
+      return(<button className="navbar-button" id="login-button">Login</button>)
+    } else {
+      return(
+        <Fragment>
+          <button className="navbar-button">User Page</button>
+          <button className="navbar-button" id="logout-button">Logout</button>
+        </Fragment>
+      )
+    }
+  }
+
   return (
     <div className="navbar">
-      Navbar Content
+      <button onClick={props.onNavbarClick} className="navbar-button" id="articles-button">Articles</button>
+      {displayUserActionsPanel()}
     </div>
   )
 }
